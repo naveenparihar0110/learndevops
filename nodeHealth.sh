3e1ef27550ca14e5bf67933fb8dc9dd94ec47ff8
naveenparihar0110 <naveenparihar0110@gmail.com>
2025-11-02 20:28:25 +0000

    node health check script added

---
+++ nodeHealth.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+#######################
+#Author: Naveen
+#Date : 02/11/2025
+#This script outputs the Node Health
+#Version: v1
+#######################
+
+echo "my name is naveen"
+
+set -x #debug mode
+set -e #exit the script when error
+set -o #pipefail
+
+df -h
+
+free -g
+
+nproc
+
+
+#mmnanan
+
+ps -ef |grep Ubuntu | awk -F" " '{print $2}'
+
+a=4
+b=10
+if [ $a -gt $b ]
+then
+echo "a is greater than b"
+else
+echo "b is greater than a"
+fi
+
+for i in {1..10};
+do echo $i;
+done
+
+
+
+
